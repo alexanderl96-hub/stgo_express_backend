1ebffc7989a8cb2312f4745fae218db494e31e46
Alexander <alexanderl96@yahoo.com>
2026-06-10 00:39:54 -0400

    Fixing path for image

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -10,199 +10,286 @@ import {
    CREATE ORDER
 ========================================= */
 
-export const createOrder = async (
-  req,
-  res
-) => {
+// export const createOrder = async (
+//   req,
+//   res
+// ) => {
 
+//   try {
+
+//     const {
+
+//       email,
+
+//       orders,
+
+//       payment_format = orders[0].payment_format,
+
+//       payment_option = orders[0].payment_option,
+
+//       adm_in_charge = orders[0].adm_in_charge,
+
+//       gestor_sell = orders[0].gestor_sell,
+
+//       seller_cash = orders[0].seller_cash,
+
+//       dollar_price = orders[0].dollar_price,
+
+//       cup_price = orders[0].cup_price,
+
+//       revenew_total = orders[0].revenew_total,
+
+//       tienda = orders[0].tienda,
+
+//       qrcode,
+
+//       status_sell = orders[0].status_sell,
+
+//       phone
+
+//     } = req.body;
+
+
+//     // FIND USER
+//     const userResult =
+//       await pool.query(
+//         `
+//         SELECT *
+//         FROM customers
+//         WHERE email = $1
+//         `,
+//         [email]
+//       );
+
+
+
+//     if (
+//       userResult.rows.length === 0
+//     ) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "User not found"
+//       });
+//     }
+
+
+
+//     const user =
+//       userResult.rows[0];
+
+//     // CREATE ORDER OBJECT
+//     const newOrder =
+//       createNewOrder({
+
+//         orders,
+
+//         payment_format,
+
+//         payment_option,
+
+//         adm_in_charge,
+
+//         gestor_sell,
+
+//         seller_cash,
+
+//         dollar_price,
+
+//         cup_price,
+
+//         revenew_total,
+
+//         tienda,
+
+//         qrcode,
+
+//         status_sell,
+
+//         phone 
+//       });
+
+
+
+//     // INSERT ORDER
+//     const result =
+//       await pool.query(
+//         `
+//         INSERT INTO orders (
+
+//           id,
+
+//           customer_id,
+
+//           qrcode,
+
+//           adm_in_charge,
+
+//           gestor_sell,
+
+//           orders,
+
+//           dollar_price,
+
+//           cup_price,
+
+//           revenew_total,
+
+//           seller_cash,
+
+//           tienda,
+
+//           phone,
+
+//           date,
+
+//           payment_format,
+
+//           payment_option,
+
+//           status_sell
+
+//         )
+//         VALUES (
+
+//           $1, $2, $3, $4,
+//           $5, $6, $7, $8,
+//           $9, $10, $11, $12,
+//           $13, $14, $15, $16
+
+//         )
+//         RETURNING *
+//         `,
+//         [
+
+//           newOrder.id,
+
+//           user.customer_id,
+
+//           newOrder.qrcode,
+
+//           newOrder.adm_in_charge,
+
+//           newOrder.gestor_sell,
+
+//           JSON.stringify(
+//             newOrder.orders
+//           ),
+
+//           newOrder.dollar_price,
+
+//           newOrder.cup_price,
+
+//           newOrder.revenew_total,
+
+//           newOrder.seller_cash,
+
+//           newOrder.tienda,
+
+//           newOrder.phone,
+
+//           newOrder.date,
+
+//           newOrder.payment_format,
+
+//           newOrder.payment_option,
+
+//           newOrder.status_sell
+//         ]
+//       );
+
+
+
+//     return res.status(201).json({
+
+//       success: true,
+
+//       order: result.rows[0]
+//     });
+
+//   } catch (error) {
+
+//     console.log(error);
+
+//     return res.status(500).json({
+
+//       success: false,
+
+//       error: error.message
+//     });
+//   }
+// };
+export const createOrder = async (req, res) => {
   try {
 
     const {
-
-      email,
-
-      orders,
-
-      payment_format = orders[0].payment_format,
-
-      payment_option = orders[0].payment_option,
-
-      adm_in_charge = orders[0].adm_in_charge,
-
-      gestor_sell = orders[0].gestor_sell,
-
-      seller_cash = orders[0].seller_cash,
-
-      dollar_price = orders[0].dollar_price,
-
-      cup_price = orders[0].cup_price,
-
-      revenew_total = orders[0].revenew_total,
-
-      tienda = orders[0].tienda,
-
+      id,
       qrcode,
-
-      status_sell = orders[0].status_sell,
-
-      phone
-
+      adm_in_charge,
+      gestor_sell,
+      orders,
+      dollar_price,
+      cup_price,
+      revenew_total,
+      seller_cash,
+      tienda,
+      guest_name,
+      guest_email,
+      phone,
+      payment_format,
+      payment_option,
+      status_sell
     } = req.body;
 
-
-    // FIND USER
-    const userResult =
-      await pool.query(
-        `
-        SELECT *
-        FROM customers
-        WHERE email = $1
-        `,
-        [email]
-      );
-
-
-
-    if (
-      userResult.rows.length === 0
-    ) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found"
-      });
-    }
-
-
-
-    const user =
-      userResult.rows[0];
-
-    // CREATE ORDER OBJECT
-    const newOrder =
-      createNewOrder({
-
-        orders,
-
-        payment_format,
-
-        payment_option,
-
-        adm_in_charge,
-
-        gestor_sell,
-
-        seller_cash,
-
-        dollar_price,
-
-        cup_price,
-
-        revenew_total,
-
-        tienda,
-
+    const result = await pool.query(
+      `
+      INSERT INTO guest_orders (
+        id,
         qrcode,
-
-        status_sell,
-
-        phone 
-      });
-
-
-
-    // INSERT ORDER
-    const result =
-      await pool.query(
-        `
-        INSERT INTO orders (
-
-          id,
-
-          customer_id,
-
-          qrcode,
-
-          adm_in_charge,
-
-          gestor_sell,
-
-          orders,
-
-          dollar_price,
-
-          cup_price,
-
-          revenew_total,
-
-          seller_cash,
-
-          tienda,
-
-          phone,
-
-          date,
-
-          payment_format,
-
-          payment_option,
-
-          status_sell
-
-        )
-        VALUES (
-
-          $1, $2, $3, $4,
-          $5, $6, $7, $8,
-          $9, $10, $11, $12,
-          $13, $14, $15, $16
-
-        )
-        RETURNING *
-        `,
-        [
-
-          newOrder.id,
-
-          user.customer_id,
-
-          newOrder.qrcode,
-
-          newOrder.adm_in_charge,
-
-          newOrder.gestor_sell,
-
-          JSON.stringify(
-            newOrder.orders
-          ),
-
-          newOrder.dollar_price,
-
-          newOrder.cup_price,
-
-          newOrder.revenew_total,
-
-          newOrder.seller_cash,
-
-          newOrder.tienda,
-
-          newOrder.phone,
-
-          newOrder.date,
-
-          newOrder.payment_format,
-
-          newOrder.payment_option,
-
-          newOrder.status_sell
-        ]
-      );
-
-
+        adm_in_charge,
+        gestor_sell,
+        orders,
+        dollar_price,
+        cup_price,
+        revenew_total,
+        seller_cash,
+        tienda,
+        guest_name,
+        guest_email,
+        phone,
+        payment_format,
+        payment_option,
+        status_sell
+      )
+      VALUES (
+        $1,$2,$3,$4,
+        $5,$6,$7,$8,
+        $9,$10,$11,$12,
+        $13,$14,$15,$16
+      )
+      RETURNING *
+      `,
+      [
+        id,
+        qrcode,
+        adm_in_charge,
+        gestor_sell,
+        JSON.stringify(orders),
+        dollar_price,
+        cup_price,
+        revenew_total,
+        seller_cash,
+        tienda,
+        guest_name,
+        guest_email,
+        phone,
+        payment_format,
+        payment_option,
+        status_sell
+      ]
+    );
 
     return res.status(201).json({
-
       success: true,
-
       order: result.rows[0]
     });
 
@@ -211,9 +298,7 @@ export const createOrder = async (
     console.log(error);
 
     return res.status(500).json({
-
       success: false,
-
       error: error.message
     });
   }
